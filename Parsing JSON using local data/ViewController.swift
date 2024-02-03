61b4cfb5d69e115b9340ce9172dde762ce57738a
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-02-03 19:26:13 +0100

    Initial Commit

---
+++ Parsing JSON using local data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parsing JSON using local data
+//
+//  Created by Macbook Air 2017 on 3. 2. 2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
